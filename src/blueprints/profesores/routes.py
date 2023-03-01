40d7d0e7edc981b1732d065899e87e9683ca086f
Marcelo Sosa <sosamarcelo92@gmail.com>
2023-03-01 00:01:08 -0300

    Merge completo

--- src/blueprints/profesores/routes.py
+++ src/blueprints/profesores/routes.py
@@ -2,31 +2,16 @@ from src.blueprints.profesores import bp
 from flask import redirect, url_for, render_template
 from flask_login import login_required, current_user
 
-
-    
 @bp.route('/')
-@login_required
 def bienvenido_profesor():
+    print(current_user.rol)
     if current_user.rol == 'profesor':
          return render_template('profesores/bienvenida.html')
     else:
         return redirect(url_for('padres.padres_home'))
 
-# endopoint para el menu de profesores que recibe el curso en string
-@bp.route('/profesores/<curso>')
-def menu_profe(curso):
-    # return "este es el curso: {0}".format(curso)
-    return render_template('profesores/menu-profe.html')
-
-@bp.route('/profesores/<curso>/crear-aviso')
-def crear_aviso(curso):
-    # return "este es el curso: {0}".format(curso)
-
-    # recibir datos del form
-
-    # insertar los datos a la bd, en la tabla avisos
-
-
+@bp.route('/crear-aviso')
+def crear_aviso():
     return render_template('profesores/crear-aviso.html')
 
 @bp.route('/estudiantes-avisos')
